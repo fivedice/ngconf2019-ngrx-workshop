b42166f85abc9850c968bf95ad00af573a779f9e
Jared Youtsey <github@fivedice.net>
2019-04-29 11:19:09 -0600

    pt 1 without entity

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -2,12 +2,13 @@ import { createEntityAdapter, EntityAdapter, EntityState } from "@ngrx/entity";
 import { Book } from "src/app/shared/models/book.model";
 import { Action } from "@ngrx/store";
 
-const adapter = createEntityAdapter({
-  selectId: (book: Book) => book.id,
-  sortComparer: (a: Book, b: Book) => a.name.localeCompare(b.name)
-});
+// const adapter = createEntityAdapter({
+//   selectId: (book: Book) => book.id,
+//   sortComparer: (a: Book, b: Book) => a.name.localeCompare(b.name)
+// });
 
-export interface State extends EntityState<Book> {
+export interface BookState {
+  //} extends EntityState<Book> {
   activeBookId: string | null;
   books: Book[];
 }
@@ -33,10 +34,15 @@ const initialBooks: Book[] = [
   }
 ];
 
-export const initialState: State = adapter.getInitialState({
+// export const initialState: State = adapter.getInitialState({
+//   activeBookId: null,
+//   books: initialBooks
+// });
+
+export const initialState: BookState = {
   activeBookId: null,
   books: initialBooks
-});
+};
 
 const createBook = (books: Book[], book: Book) => [...books, book];
 const updateBook = (books: Book[], book: Book) =>
@@ -46,7 +52,7 @@ const updateBook = (books: Book[], book: Book) =>
 const deleteBook = (books: Book[], book: Book) =>
   books.filter(w => book.id !== w.id);
 
-export function reducer(state = initialState, action: Action) {
+export function reducer(state = initialState, action: Action): BookState {
   switch (action.type) {
     default:
       return state;
